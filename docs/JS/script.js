8a22800684c7aee44e794abaf3ce7f08e66190f2
BSIT3E-CARBONILLA <202202776@cityofmalabonuniversity.edu.ph>
2024-11-26 09:28:35 +0800

    PORTFOLIO

---
+++ docs/JS/script.js
@@ -0,0 +1,14 @@
+function showText(id) {
+    const textElement = document.getElementById(id);
+    textElement.classList.toggle("hidden");
+  }
+  
+  
+  function navigateTo(page) {
+    window.location.href = page;
+  }
+  
+  function navigateTo(url) {
+    window.location.href = url;
+  }
+  
